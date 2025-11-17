4949dfc38f6bc79b0ce128963efa262a719b5f1c
Dhiraj96k <dhirajsalunke7350@gmail.com>
2025-11-17 22:09:38 +0530

    Header Section Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-
+import Header from "./Components/Header/Header";
 
 export default function App() {
 
   return <>
+    <Header />
   </>
 }
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,29 @@
+// Header.jsx
+import React from "react";
+import "./Header.css";
+
+export default function Header() {
+    return (
+        <header className="header">
+            <div className="header-container">
+                {/* Logo */}
+                <div className="logo">
+                    <div className="logo-icon"></div>
+                    <span className="logo-text">smartComplaint</span>
+                </div>
+
+                {/* Navigation */}
+                <nav className="nav">
+                    <a href="#">About</a>
+                    <a href="#">Contact Us</a>
+                </nav>
+
+                {/* Buttons */}
+                <div className="btn-group">
+                    <button className="btn primary">Raise Complaint</button>
+                    <button className="btn outline">Login</button>
+                </div>
+            </div>
+        </header>
+    );
+}
